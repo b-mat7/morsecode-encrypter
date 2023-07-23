4e36600b91caeeb16974322648639be498483fa4
b-mat7 <121493355+b-mat7@users.noreply.github.com>
2023-07-24 00:34:29 +0200

    Project finished

---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+"use strict"
+
+const inputBtn = document.body.querySelector("button");
+
+const morseAlphabet = [
+  { letter: "1", morseCode: ".----" },
+  { letter: "2", morseCode: "..---" },
+  { letter: "3", morseCode: "...--" },
+  { letter: "4", morseCode: "....-" },
+  { letter: "5", morseCode: "....." },
+  { letter: "6", morseCode: "-...." },
+  { letter: "7", morseCode: "--..." },
+  { letter: "8", morseCode: "---.." },
+  { letter: "9", morseCode: "----." },
+  { letter: "0", morseCode: "-----" },
+  { letter: " ", morseCode: "    " },
+  { letter: "A", morseCode: ".-" },
+  { letter: "B", morseCode: "-..." },
+  { letter: "C", morseCode: "-.-." },
+  { letter: "D", morseCode: "-.." },
+  { letter: "E", morseCode: "." },
+  { letter: "F", morseCode: "..-." },
+  { letter: "G", morseCode: "--." },
+  { letter: "H", morseCode: "...." },
+  { letter: "I", morseCode: ".." },
+  { letter: "J", morseCode: ".---" },
+  { letter: "K", morseCode: "-.-" },
+  { letter: "L", morseCode: ".-.." },
+  { letter: "M", morseCode: "--" },
+  { letter: "N", morseCode: "-." },
+  { letter: "O", morseCode: "---" },
+  { letter: "P", morseCode: ".--." },
+  { letter: "Q", morseCode: "--.-" },
+  { letter: "R", morseCode: ".-." },
+  { letter: "S", morseCode: "..." },
+  { letter: "T", morseCode: "-" },
+  { letter: "U", morseCode: "..-" },
+  { letter: "V", morseCode: "...-" },
+  { letter: "W", morseCode: ".--" },
+  { letter: "X", morseCode: "-..-" },
+  { letter: "Y", morseCode: "-.--" },
+  { letter: "Z", morseCode: "--.." }
+];
+
+const encrypt = () => {
+  const inputField = document.body.querySelector("textarea");
+  const outputContainer = document.body.querySelector("p");
+
+  outputContainer.textContent = "";
+
+  const inputMsg = inputField.value.toLocaleUpperCase().split("");
+  let outputCode = [];
+
+  inputMsg.forEach((char) => {
+    morseAlphabet.forEach((code) => {
+      if(char === code["letter"]) {
+        outputCode.push(code["morseCode"]);
+      }
+    })
+  })
+
+  let outputMsg = outputCode.join(" ");
+
+  for(let i = 0; i < outputMsg.length ; i++){
+    setTimeout(() => {
+      outputContainer.textContent += outputMsg[i];
+    }, 200 * i)
+  }
+}
+
+inputBtn.addEventListener("click", encrypt);
+
